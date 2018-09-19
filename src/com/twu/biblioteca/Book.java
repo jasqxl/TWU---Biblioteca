a171965b76e263a1de2a6ce5b1357c1b2b142ba0
JASMINE KUAH <jasminekuah@JASMINEs-MacBook-Pro.local>
2018-09-19 23:39:26 +0800

    Added Menu and Book class

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -1,6 +1,7 @@
 package com.twu.biblioteca;
 
 public class Book {
+
     private String title;
     private String author;
     private int publishYear;
--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -1,10 +1,18 @@
 package com.twu.biblioteca;
 
+import java.io.*;
+import java.util.*;
+
 public class Menu {
-    private static String welcomeMessage = "Welcome to Biblioteca :)";;
-    private static String menuHeading = "Please choose an action from the list below:";;
-    private static String option1 = "1) List Books";;
-    private static String menuHeadhing = "Please choose an action from the list below:";;
+
+    private static String welcomeMessage = "Welcome to Biblioteca :)\n";
+    private static String menuHeading = "Please choose an action from the list below:";
+
+    private static List<String> Options = new ArrayList<String>();
+    private static String fileName = "Options.txt";
+    private static String line = null;
+
+    //private static String workingOptionFilePath = System.getProperty("user.dir") + "/Options.txt";
 
     public static void getWelcomeMessage() {
         System.out.println(welcomeMessage);
@@ -12,6 +20,41 @@ public class Menu {
 
     public static void showMenu() {
         System.out.println(menuHeading);
-        System.out.println(menuHeading);
+        getOptions();
+    }
+
+    public static void getOptions () {
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while((line = bufferedReader.readLine()) != null) {
+                Options.add(line);
+                System.out.println(Options.size() + ") " + Options.get(Options.size()-1));
+            }
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            writeToOptionsFile("List Books");
+            getOptions();
+        }
+        catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
+    }
+
+    private static void writeToOptionsFile(String newOption) {
+        try (FileWriter fileWriter = new FileWriter(fileName, true);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+             PrintWriter printWriter = new PrintWriter(bufferedWriter)) {
+            printWriter.println(newOption);
+        }
+        catch(IOException ex1) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
+        Options.clear();
+    }
+
+    public static void addOption(String newOptionToAdd) {
+        writeToOptionsFile(newOptionToAdd);
     }
 }
